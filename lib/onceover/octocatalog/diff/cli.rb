6449259f87939fad34ed19d410fdaa84e5e45295
Mitchell Chambers <mitchell.chambers@puppet.com>
2023-06-27 16:12:40 +1000

    include private key file

--- lib/onceover/octocatalog/diff/cli.rb
+++ lib/onceover/octocatalog/diff/cli.rb
@@ -94,7 +94,7 @@ class Onceover
                     #'remote' => repo.root,
                     #'remote' => @git_remote[0][:stdout],
                     'remote' => 'git@github.com:chambersmp/control-repo.git',
-                    # 'private_key' => '/root/.ssh/id_ed25519',
+                    'private_key' => '/root/.ssh/id_ed25519',
                     'basedir' => environment_dir,
                     'invalid_branches' => 'correct_and_warn'
                   },
